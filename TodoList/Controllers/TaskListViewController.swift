893254581dfbcc99398873c1fb88ae5e14c3c19e
Paulo Junior <jr@live.at>
2021-08-18 16:07:23 -0300

    Designing the Task List Screen

---
+++ TodoList/Controllers/TaskListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskListViewController.swift
+//  TodoList
+//
+//  Created by Junior Ferreira on 18/08/21.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+}
