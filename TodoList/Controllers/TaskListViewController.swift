e4c3058ab52a29892a163edaf15b40cd515effa9
Paulo Junior <jr@live.at>
2021-08-19 16:57:32 -0300

    Adding Tasks Using Variable

--- TodoList/Controllers/TaskListViewController.swift
+++ TodoList/Controllers/TaskListViewController.swift
@@ -14,6 +14,8 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
+    private var tasks = Variable<[Task]>([])
+    
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -45,7 +47,7 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
         
         addTVC.taskSubjectObservable
             .subscribe(onNext: { task in
-                print(task)
+                self.tasks.value.append(task)
             }).disposed(by: disposeBag)
     }
 }
